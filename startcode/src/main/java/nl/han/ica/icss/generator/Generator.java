70489bc94cd5ac4a10cdc105865357fe95d871f0
Lodewijk Klaassen <11039lodewijk@gmail.com>
2025-03-26 19:21:24 +0100

    Generator done

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -2,14 +2,47 @@ package nl.han.ica.icss.generator;
 
 
 import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.Declaration;
+import nl.han.ica.icss.ast.Stylerule;
+import nl.han.ica.icss.ast.Stylesheet;
+import nl.han.ica.icss.ast.literals.ColorLiteral;
+import nl.han.ica.icss.ast.literals.PercentageLiteral;
+import nl.han.ica.icss.ast.literals.PixelLiteral;
 
 public class Generator {
 
-	public String generate(AST ast) {
-        return "";
+    StringBuilder stringBuilder = new StringBuilder();
+
+    public String generate(AST ast) {
+        generateStylesheet(ast.root);
+        return stringBuilder.toString();
+    }
+
+    private void generateStylesheet(Stylesheet stylesheet) {
+        stylesheet.body.forEach(stylerule -> generateStylerule((Stylerule) stylerule));
+    }
+
+    private void generateStylerule(Stylerule stylerule) {
+        stylerule.selectors.forEach(selector -> stringBuilder.append(selector.toString()).append(" "));
+        stringBuilder.append("{\n");
+        stylerule.body.forEach(declaration -> generateDeclaration((Declaration) declaration));
+        stringBuilder.append("}\n\n");
+    }
+
+    private void generateDeclaration(Declaration declaration) {
+        stringBuilder
+                .append("\t")
+                .append(declaration.property.name)
+                .append(": ")
+                .append(switch (declaration.expression.expressionType) {
+                    case PERCENTAGE -> ((PercentageLiteral) declaration.expression).value + "%";
+                    case PIXEL -> ((PixelLiteral) declaration.expression).value + "px";
+                    case COLOR -> ((ColorLiteral) declaration.expression).value;
+                    default ->
+                            throw new IllegalStateException("Unexpected value: " + declaration.expression.expressionType);
+                })
+                .append(";\n");
+    }
 
 
-	}
-
-	
 }
