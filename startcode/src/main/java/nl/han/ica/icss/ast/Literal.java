e89c3ade80468ed854de776dc0a644b9130773bb
Lodewijk Klaassen <11039lodewijk@gmail.com>
2025-03-27 11:05:04 +0100

    Disallowed changing the expressionType of Literals
    (also need not be public?)

--- startcode/src/main/java/nl/han/ica/icss/ast/Literal.java
+++ startcode/src/main/java/nl/han/ica/icss/ast/Literal.java
@@ -1,4 +1,10 @@
 package nl.han.ica.icss.ast;
 
+import nl.han.ica.icss.ast.types.ExpressionType;
+
 public abstract class Literal extends Expression {
+    @Override
+    public ExpressionType setExpressionType(ExpressionType expressionType) {
+        throw new IllegalAccessError("Not allowed to change the expressionType of Literal");
+    }
 }
